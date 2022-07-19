06727c2da602016da720c5608e28498dad641589
Hector <avelar.h@yahoo.com>
2022-07-19 14:14:13 -0500

    a

---
+++ modules/exhibit.php
@@ -0,0 +1,53 @@
+<div class="container">
+<div class="row">
+<div class="col-md-4">
+    <img src="img/placeholderRAT.jpg" alt="" width="100%"/>
+</div>
+<div class="col-md-8">
+    <div class="row">
+        <h1>This is a title</h1>
+    </div>
+    <div class="row">
+        rating
+    </div>
+    <div class="row">
+    <div class="col-6">
+        <h2>Precio</h2>
+    </div>
+    <div class="col-6">
+        <h2>$cash</h2>
+    </div>
+    <div class="d-none"></div>
+    </div>
+    <div class="row">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam laboriosam quo officiis ipsa neque quis, iure, voluptas alias ratione omnis consequuntur. Vel, modi consequuntur voluptate ullam nihil fugiat optio libero?</p>
+    </div>
+    <div class="row">
+        talla
+    </div>
+    <div class="row">
+        <button class="btn btn-info">Comprar Ahora</button>
+    </div>
+</div>
+</div>
+
+
+<div class="row">
+    <div class="col-md-3">
+    <div class="row">Estilo</div>
+    <div class="row">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic ex porro quisquam soluta amet animi sed vitae quos laudantium tenetur, odit harum id deleniti quas aut vero est quasi necessitatibus.</div>
+    </div>
+    <div class="col-md-3">
+    <div class="row">Género</div>
+    <div class="row">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga hic beatae, consectetur quisquam amet corrupti quae voluptatibus laborum, quibusdam optio autem doloribus voluptates. Laboriosam quae iusto porro, maxime omnis labore?</div>
+    </div>
+    <div class="col-md-3">
+    <div class="row">Marca</div>
+    <div class="row">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt eaque magni sequi suscipit non, deserunt ducimus obcaecati expedita neque beatae amet fuga quis! Nemo eveniet in quas facilis! At, iusto.</div>
+    </div>
+    <div class="col-md-3">
+    <div class="row">Color</div>
+    <div class="row">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti expedita rem ab sunt explicabo earum itaque facilis, praesentium dolores vel doloremque illo eius harum eaque debitis sequi! Voluptate, deserunt explicabo.</div>
+    </div>
+</div>
+</div>
