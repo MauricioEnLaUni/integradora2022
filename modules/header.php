918a8b2657525cf26423ca4e4a02eaea50c78443
MauricioEnLaUni <210804@utags.edu.mx>
2022-07-23 17:14:31 -0500

    Working on header

--- modules/header.php
+++ modules/header.php
@@ -1,56 +1,119 @@
-<div class="container-fluid navContainer">
-<div class="row">
-  <div class="col-md-3 g-0">
-    <a href="index.php"><div id="logo"></div></a>
-  </div>
-  <div class="col-md-9">
-    <div class="row flex-row-reverse" id="upperBar">
-    <div id="inscribir" class="noneBasic col-md-2">
-      <img src="img/svg/pencil-square.svg" alt="" />
-      <a
-      class="btn btn-link logButtons"
-      data-bs-toggle="modal"
-      href="#logInModal2"
-      role="button">
-          Inscribirse
-      </a>
+<header>
+  <div class="container-fluid g-0">
+    <div class="row">
+      <div class="col-md-3 col-3 g-0" id="logoCont">
+        <a href="index.php">
+          <div id="logo"></div>
+        </a>
+      </div>
+      <nav class="col-md-9 d-md-block col-9 g-0">
+        <div class="row flex-md-row-reverse g-0">
+          <div id="hButtons" class="d-md-none d-flex row justify-content-around">
+            <button
+            class="btn btn-primary d-md-none col-3 d-flex"
+            type="button"
+            data-bs-toggle="modal"
+            role="button"
+            href="#logInModal2">
+              <img src="img/svg/personFill.svg" alt="" />
+            </button>
+            <button
+            class="btn btn-primary d-md-none col-3 d-flex"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvas"
+            aria-controls="offcanvas">
+              <img src="img/svg/list.svg" alt="" />
+            </button>
+            <button
+            class="btn btn-primary d-md-none col-3 d-flex"
+            type="button"
+            data-bs-toggle="modal"
+            role="button"
+            href="#searchModal">
+              <img src="img/svg/search.svg" alt="" />
+            </button>
+            <button
+            class="btn btn-primary d-md-none col-3 d-flex"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasScrolling"
+            aria-controls="offcanvasScrolling">
+              <img src="img/svg/list.svg" alt="" />
+            </button>
+          </div>
+          <div id="inscribir" class="noneBasic d-none d-md-block col-md-2">
+            <img src="img/svg/pencil-square.svg" alt="" />
+            <a
+            class="btn btn-link logButtons"
+            data-bs-toggle="modal"
+            href="#logInModal2"
+            role="button">
+                Inscribirse
+            </a>
+          </div>
+          <div id="acceder" class="noneBasic d-none d-md-block col-md-2">
+            <img src="img/svg/personFill.svg" alt="" />
+            <a
+            class="btn btn-link logButtons"
+            data-bs-toggle="modal"
+            href="#logInModal"
+            role="button">
+                Acceder
+            </a>
+          </div>
+          <div class="search-bar col-md-4 d-none d-md-block">
+            <input
+                type="search"
+                class="search-bar__input"
+                placeholder="Buscar"
+                aria-label="buscar"
+            />
+            <button type="submit" class="search-bar__submit" aria-label="enviar búsqueda">
+                <img src="img/svg/search.svg" alt="" />
+            </button>
+          </div>
+        </div>
+        <div class="row d-none d-md-flex text-center navBCont g-0">
+          <a class="navButtons col-md-3" href="index.php">
+            INICIO
+          </a>
+          <a class="navButtons col-md-3" href="">
+            NOVEDADES
+          </a>
+          <a class="navButtons col-md-3" href="about.php">
+            NOSOTROS
+          </a>
+          <a class="navButtons col-md-3" href="contact.php">
+            CONTACTO
+          </a>
+        </div>
+      </nav>
     </div>
-    <div id="acceder" class="noneBasic col-md-2">
-      <img src="img/svg/personFill.svg" alt="" />
-      <a
-      class="btn btn-link logButtons"
-      data-bs-toggle="modal"
-      href="#logInModal"
-      role="button">
-          Acceder
-      </a>
-    </div>
-    <div class="search-bar col-md-4">
-      <input
-          type="search"
-          class="search-bar__input"
-          placeholder="Buscar"
-          aria-label="buscar"
-      />
-      <button type="submit" class="search-bar__submit" aria-label="enviar búsqueda">
-          <img src="img\svg\search.svg" alt="" />
-      </button>
-    </div>
   </div>
-  <div class="row text-center navBCont">
-    <a class="navButtons col-3" href="index.php">
-      INICIO
-    </a>
-    <a class="navButtons col-3" href="">
-      NOVEDADES
-    </a>
-    <a class="navButtons col-3" href="about.php">
-      NOSOTROS
-    </a>
-    <a class="navButtons col-3" href="contact.php">
-      CONTACTO
-    </a>
+</header>
+<div
+class="offcanvas offcanvas-end g-0 d-md-none"
+data-bs-scroll="true"
+data-bs-backdrop="false"
+tabindex="-1"
+id="offcanvasScrolling"
+aria-labelledby="offcanvasScrollingLabel"
+>
+  <div class="offcanvas-header">
+    <button
+    type="button"
+    class="btn-close"
+    data-bs-dismiss="offcanvas"
+    aria-label="Close">
+    </button>
   </div>
-</div>
-</div>
+  <div class="offcanvas-body row text-center navBCont g-0">
+    <ul>
+      <li><a href="" class="navList">INICIO</a></li>
+      <li><a href="" class="navList">NOVEDADES</a></li>
+      <li><a href="" class="navList">NOSOTROS</a></li>
+      <li><a href="" class="navList">CONTACTO</a></li>
+    </ul>
+  </div>
 </div>
